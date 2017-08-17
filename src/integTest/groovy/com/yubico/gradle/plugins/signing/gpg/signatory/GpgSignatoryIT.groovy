74fb6bed73f7e80fdb1750ec0ed6bf2d0b1bbd09
Emil Lundberg <emil@yubico.com>
2017-08-17 18:13:56 +0200

    Add integration test for GpgSignatory

---
+++ src/integTest/groovy/com/yubico/gradle/plugins/signing/gpg/signatory/GpgSignatoryIT.groovy
@@ -0,0 +1,48 @@
+package com.yubico.gradle.plugins.signing.gpg.signatory
+
+import org.apache.commons.io.IOUtils
+import org.gradle.plugins.ide.eclipse.model.Output
+import org.gradle.plugins.signing.signatory.Signatory
+import org.junit.Rule
+import org.junit.rules.TemporaryFolder
+import spock.lang.Specification
+
+class GpgSignatoryIT extends Specification {
+
+  private static final String KEY_ID = "B84D477DA20D6764"
+
+  @Rule
+  public final TemporaryFolder tmpDir = new TemporaryFolder();
+
+  def 'Signature is written correctly.'() {
+    when:
+      URL payload = getClass().getResource("/payload.txt")
+      File signatureFile = tmpDir.newFile("payload.txt.sig")
+
+      println("Writing signature to: ${signatureFile}")
+      println("Payload in: ${payload.getPath()}")
+      println("Payload: ${payload.openStream().text}")
+      println("Using GNUPGHOME in: ${getClass().getResource("/gnupg").getPath()}")
+
+      Signatory gpgSignatory = new GpgSignatory(KEY_ID, new File(getClass().getResource("/gnupg").toURI()));
+
+
+      OutputStream signatureStream = new FileOutputStream(signatureFile)
+
+      gpgSignatory.sign(getClass().getResourceAsStream("/payload.txt"), signatureStream)
+
+      ProcessBuilder gpgProcessBuilder = new ProcessBuilder("gpg", "--verify", signatureFile.getAbsolutePath(), payload.getPath())
+        .redirectErrorStream(true)
+
+      gpgProcessBuilder.environment().put("GNUPGHOME", getClass().getResource("/gnupg").getPath())
+
+      Process gpgProcess = gpgProcessBuilder.start()
+      gpgProcess.waitFor()
+
+      String gpgOutput = IOUtils.toString(gpgProcess.getInputStream(), "UTF-8")
+
+    then:
+      gpgOutput.contains('Good signature from "Test Testsson <test@test.org>"')
+  }
+
+}
--- src/main/java/com/yubico/gradle/plugins/signing/gpg/signatory/GpgSignatory.java
+++ src/main/java/com/yubico/gradle/plugins/signing/gpg/signatory/GpgSignatory.java
@@ -1,21 +1,32 @@
 package com.yubico.gradle.plugins.signing.gpg.signatory;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
 import org.apache.commons.io.IOUtils;
 import org.gradle.plugins.signing.signatory.SignatorySupport;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 class GpgSignatory extends SignatorySupport {
 
-    private final String keyId;
+    private static final Logger logger = LoggerFactory.getLogger(GpgSignatory.class);
 
-    public GpgSignatory(String keyId) {
+    private final String keyId;
+    private final File gpgHome;
+
+    GpgSignatory(String keyId, File gpgHome) {
         if (keyId == null || keyId.isEmpty()) {
             throw new IllegalArgumentException("keyId must not be null.");
         }
         this.keyId = keyId;
+        this.gpgHome = gpgHome;
+    }
+
+    public GpgSignatory(String keyId) {
+        this(keyId, null);
     }
 
     @Override
@@ -26,8 +37,14 @@ class GpgSignatory extends SignatorySupport {
     @Override
     public void sign(InputStream toSign, OutputStream destination) {
         try {
-            Process gpgProcess = new ProcessBuilder("gpg", "--local-user", keyId, "--detach-sign", "--use-agent").start();
+            ProcessBuilder gpgProcessBuilder = new ProcessBuilder("gpg", "--local-user", keyId, "--detach-sign");
 
+            if (gpgHome != null) {
+                logger.debug("Using GNUPGHOME: " + gpgHome.getAbsolutePath());
+                gpgProcessBuilder.environment().put("GNUPGHOME", gpgHome.getAbsolutePath());
+            }
+
+            Process gpgProcess = gpgProcessBuilder.start();
             IOUtils.copy(toSign, gpgProcess.getOutputStream());
             gpgProcess.getOutputStream().close();
 
