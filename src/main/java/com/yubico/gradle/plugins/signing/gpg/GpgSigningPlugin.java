a8e1c4843bc3456257073bdc28cadb00f4352845
Minecrell <dev@minecrell.net>
2017-09-22 20:49:49 +0200

    Add workaround to make plugin work for current Gradle versions
    Avoids the runtime cast to PgpSignatory by replacing the property
    when the GpgSigningPlugin is applied. Plugin works fine without
    a patch on Gradle 4.1 and 4.2.

--- src/main/java/com/yubico/gradle/plugins/signing/gpg/GpgSigningPlugin.java
+++ src/main/java/com/yubico/gradle/plugins/signing/gpg/GpgSigningPlugin.java
@@ -1,10 +1,15 @@
 package com.yubico.gradle.plugins.signing.gpg;
 
 import com.yubico.gradle.plugins.signing.gpg.signatory.GpgSignatoryProvider;
+import org.gradle.api.Action;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
+import org.gradle.plugins.signing.Sign;
 import org.gradle.plugins.signing.SigningExtension;
 import org.gradle.plugins.signing.SigningPlugin;
+import org.gradle.plugins.signing.signatory.Signatory;
+
+import java.util.concurrent.Callable;
 
 public class GpgSigningPlugin implements Plugin<Project> {
 
@@ -13,6 +18,18 @@ public class GpgSigningPlugin implements Plugin<Project> {
     project.getPluginManager().apply(SigningPlugin.class);
 
     project.getExtensions().getByType(SigningExtension.class).setSignatories(new GpgSignatoryProvider());
+    project.getTasks().withType(Sign.class, new Action<Sign>() {
+      @Override
+      public void execute(Sign sign) {
+        // Workaround bug that prevents using custom signatories
+        sign.getInputs().property("signatory", new Callable<String>() {
+          @Override
+          public String call() throws Exception {
+            return sign.getSignatory().getName();
+          }
+        });
+      }
+    });
   }
 
 }
